c6f73aa9b32e4f9f6c50d164068204715da55e1f
Fraser Molyneux <fmolyneux@microsoft.com>
2024-10-06 09:55:29 +0100

    Updating template

--- terraform/providers.tf
+++ terraform/providers.tf
@@ -14,7 +14,11 @@ terraform {
 provider "azurerm" {
   subscription_id = var.subscription_id
 
-  features {}
+  features {
+    resource_group {
+      prevent_deletion_if_contains_resources = false
+    }
+  }
 
   storage_use_azuread = true
 }
